38ca9dcf3323b004b5bdbf3911aeaa2741fa6cea
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-03-08 00:01:00 -0500

    Tutorial version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,23 @@
 class PagesController < ApplicationController
 
     def index
-        require 'open-uri'
-        require 'json'
-        response = JSON.parse(open("http://clt.charlotte.opendata.arcgis.com/datasets/32526cecacdc4802bbacbdd76f246896_0.geojson").read)
+        # 1. Require json and open-uri
         
-        # Remove past closures
-        @results = response['features'].select do |elem|
-            elem['properties']['ENDDATE'] && !Date.parse(elem['properties']['ENDDATE']).past?
-        end
+        # 2. Retrieve the closure feed from Charlotte's Open Data portal
+        # Use open("url").read to fetch the feed and use JSON.parse() to convert it to an array.
+        # http://clt.charlotte.opendata.arcgis.com/datasets/32526cecacdc4802bbacbdd76f246896_0.geojson
         
-        # Sort remaining colsures
-        @results.sort! { |x,y| x['properties']['ENDDATE'] <=> y['properties']['ENDDATE'] }
+        # response = your_code_here
+        
+        # 3. Filter out closures that have expired. ENDDATE < NOW
+        # Notice that the records are within a 'features' array in the JSON. 
+        
+        # @results = response['features'].select do |elem|
+            # Keep records that have an ENDDATE and the ENDDATE has not passed yet
+        # end
+         
+        # 4. Sort remaining colsures. Records with the closest ENDDATE should be first.
+        # @results.sort! { |x,y| comparison_here }
     
     end
 end
